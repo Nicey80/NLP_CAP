a5126a49e1d7f56ae1ca3d28fb66ac7da902bd4c
Paul Nice <nicey80@gmail.com>
2018-03-05 12:40:58 +0000

    Initial Commit

---
+++ app.R
@@ -0,0 +1,56 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(tidytext)
+library(stringi)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         #sliderInput("bins",
+                     # "Number of bins:",
+                     # min = 1,
+                     # max = 50,
+                     # value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+          textInput("ti","Enter your text here"),
+          textOutput("textpred"),
+          textOutput("titoks")
+         #plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+  output$titoks <- renderText({
+    nwords <- stri_count_words(input$ti)
+    
+    nwords
+    
+  }) 
+  
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
